e95f21b44835ea09b442a8e0b8cbc2d8618cfadf
samucarafi <160398348+samucarafi@users.noreply.github.com>
2024-09-29 10:39:51 -0300

    finalização

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 const hamburguer=document.querySelector('.hamburguer')
+const imgLupa=document.querySelector('.img-lupa')
+const input=document.querySelector('.icons input')
+
+
+//função acionada ao retirar o foco do input através do onblur no HTML
+function fecharInput(){
+    input.style.display='none'
+}
 
 function clickHamburguer(img,link,navbar){
     img.src='https://img.icons8.com/ios-filled/30/FFFFFF/x.png'
     link.classList.add('aClick')
     link.classList.add('linkClick')
-    navbar.classList.add('navbarClick')
-    hamburguer.style.textAlign='end' 
+    navbar.classList.add('navbarClick') 
 }
+
 function clickX(img,link,navbar){
     img.src='https://img.icons8.com/ios-filled/50/FFFFFF/menu--v1.png'
     link.classList.remove('aClick')
@@ -14,11 +22,11 @@ function clickX(img,link,navbar){
     navbar.classList.remove('navbarClick')
 }
 
-
 hamburguer.addEventListener('click',()=>{
     const img=document.querySelector('.hamburguer img')
     const link=document.querySelector('.link')
     const navbar=document.querySelector('.navbar')
+    
 
     if(img.src=="https://img.icons8.com/ios-filled/50/FFFFFF/menu--v1.png"){
         clickHamburguer(img,link,navbar)
@@ -26,3 +34,9 @@ hamburguer.addEventListener('click',()=>{
         clickX(img,link,navbar)
     }
 })
+
+imgLupa.addEventListener('click',()=>{
+    input.style.display='inline'
+    input.focus() 
+})
+
